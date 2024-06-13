586a4f6ce2623ca7f5bd68f476bcd0c38dd1d15d
Vernon Malcolm Stuurman <19285361@sun.ac.za>
2024-06-13 07:44:15 +0200

    added some documentation to the code to make it more understandable

--- src/brainfuck.c
+++ src/brainfuck.c
@@ -1,3 +1,47 @@
+/**
+ * Brainfuck Interpreter
+ * 
+ * This program implements an interpreter for the Brainfuck programming language.
+ * The interpreter is implemented in C and consists of the following components:
+ * 
+ * 1. executeBrainfuck(const char *code): 
+ *    - This function takes Brainfuck code as input and executes it.
+ *    - It uses a tape of size 30,000 cells (initialized to zero) to store data.
+ *    - The tape is manipulated using a data pointer that can move left or right.
+ *    - The function supports all Brainfuck commands, including loops.
+ *    - The output is stored in a buffer and returned as a string.
+ * 
+ * 2. readAndExecuteFile(const char *filename):
+ *    - This function reads Brainfuck code from a specified file.
+ *    - It then calls the executeBrainfuck function to execute the code.
+ *    - The output of the execution is printed to the standard output.
+ * 
+ * 3. main(int argc, char *argv[]):
+ *    - The entry point of the program.
+ *    - It expects one command-line argument: the name of the file containing Brainfuck code.
+ *    - The function calls readAndExecuteFile to execute the Brainfuck code from the file.
+ * 
+ * Usage:
+ * 
+ * To compile the program:
+ * gcc -o brainfuck_interpreter brainfuck_interpreter.c
+ * 
+ * To run the program:
+ * ./brainfuck_interpreter <filename>
+ * 
+ * Example:
+ * ./brainfuck_interpreter hello_world.bf
+ * 
+ * The file hello_world.bf should contain valid Brainfuck code.
+ * 
+ * Note:
+ * - The program uses dynamic memory allocation and should be run on a system
+ *   with sufficient memory.
+ * - Error handling is included to manage file operations and memory allocation failures.
+ * - The program uses the standard input for the ',' command in Brainfuck, which reads a single character.
+ * 
+ * Author: Vernon Stuurman
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -31,7 +75,7 @@ char* executeBrainfuck(const char *code) {
                 (*ptr)--;
                 break;
             case '.':
-                output_buffer[output_index++] = *ptr;  // Save output to buffer
+                output_buffer[output_index++] = *ptr;
                 break;
             case ',':
                 *ptr = getchar();
